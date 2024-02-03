3b09d56ecdf7182d2d8f22b09c1170c0ac1a56c4
Oliver Dantzer <oliverdantzer@gmail.com>
2024-02-03 18:11:31 -0500

    getFollowUpQuestion

--- app/fetchBackend.js
+++ app/fetchBackend.js
@@ -15,15 +15,12 @@ export function testBackend() {
     });
 }
 
-export function testQuestion() {
+export function getFirstQuestion() {
   fetch("http://10.216.196.221:3000/api/getQuestion", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      },
-      body: {
-        "happpy": 1
-    }
+    },
   })
     .then((response) => response.json())
     .then((data) => {
@@ -35,3 +32,19 @@ export function testQuestion() {
     });
 }
 
+export function getFollowUpQuestion(previousQuestions, previousAnswers) {
+    fetch("http://10.216.196.221:3000/api/getFollowUpQuestion", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        return data.question;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        return null;
+      });
+  }
