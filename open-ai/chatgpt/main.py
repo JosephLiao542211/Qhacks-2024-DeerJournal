c6eb68563ef3dd18eb1110af198b042b9a77ee26
Evan-Ferreira <evanjfer@gmail.com>
2024-02-03 02:41:23 -0500

    finished vision board images

--- open-ai/chatgpt/main.py
+++ open-ai/chatgpt/main.py
@@ -2,6 +2,7 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
+import extra_functions
 import models
 
 api_key = os.environ.get('.env')
@@ -15,6 +16,7 @@ first_sys_r_positive = "That's great to hear!"
 first_sys_r_negative = "Oh no! I'm sorry to hear that!"
 gratitude_q = "What are three things you are grateful for today?"
 vision_month_q = "What are 3 things you want to accomplish in a month?"
+vision_board_into = "Here is your vision board for the next month:"
 
 def main():
   conversation_history = []
@@ -63,7 +65,7 @@ def main():
   print(second_follow_up_r_r)
   print(gratitude_q)
 
-  #Adds response from third_user from second follow-up question to conversation history, gratitude question, and response
+  # Adds response from third_user from second follow-up question to conversation history, gratitude question, and response
   gratitude_r = "I am grateful for my friends, my family, and my teacher."
   conversation_history.append({"role": "assistant", "content": second_follow_up_r_r})
   conversation_history.append({"role": "assistant", "content": gratitude_q})
@@ -73,7 +75,26 @@ def main():
   r_gratitude_q = models.r_gratitude_q("gpt-3.5-turbo", conversation_history)
   print(r_gratitude_q)
   print("Weekly Vision Board: " + vision_month_q)
-  
-  # Child's response to vision board question and vision board question
+
+  # Adds response from gratitude question and adds vision board question to conversation history
+  conversation_history.append({"role": "assistant", "content": r_gratitude_q})
+  conversation_history.append({"role": "assistant", "content": vision_month_q})
+
+  # Child's response to vision board question and vision board question; appends both to conversation history and introduces vision board
   vision_board_r = "I want to get better at soccer, I want to learn more about fractions, and I want to make more friends."
+  vision_board_r_goals = models.goal_differentiation("gpt-3.5-turbo", vision_board_r)
+  conversation_history.append({"role": "user", "content": vision_board_r})
+  goal_response = models.goal_response("gpt-3.5-turbo", conversation_history)
+  print(goal_response)
+  print(vision_board_into)
+
+  #Creates vision board images for user
+  vision_goals = vision_board_r_goals.split('\n')
+  image1 = models.vision_board_img(vision_goals[0])
+  image2 = models.vision_board_img(vision_goals[1])
+  image3 = models.vision_board_img(vision_goals[2])
+  extra_functions.save_display_img(image1)
+  extra_functions.save_display_img(image2)
+  extra_functions.save_display_img(image3)
+
 main()
--- open-ai/chatgpt/models.py
+++ open-ai/chatgpt/models.py
@@ -113,4 +113,53 @@ def r_gratitude_q(model, prev_conversation_history):
 
     return assistant_reply
 
+def goal_differentiation(model, user_goals):
+    # Send the entire conversation history to the model
+    response = client.chat.completions.create(
+    model=model,
+    messages=[{"role": "system", "content":
+                "You are now a professional who only reads user prompts that contain three goals that they want to accomplish\
+                in the next month. You need to find these three goals and then \
+                reiterate the user's three goals that they gave you in the following numbered list format: \
+                1. (Goal #1) \
+                2. (Goal #2) \
+                3. (Goal #3) \
+                "},
+                {"role": "user", "content": user_goals}]
+    )
+    # Extract the assistant's reply from the response
+    assistant_reply = response.choices[0].message.content
+
+    return assistant_reply
+
+def goal_response(model, prev_conversation_history):
+    new_conversation_history = []
+    new_conversation_history += prev_conversation_history
+    new_conversation_history.append({"role": "system", "content":
+                "You are still a super enthusiastic, positive, concise, and endearing journalling assistant talking to a child. \
+                Your responses are strictly limited to 1 sentence. You can not go over this limit. \
+                You just asked the user 'what are 3 things they want to accomplish this month?'.\
+                Give a very positive response to their 3 goals that they want to accomplish. \
+                "})
+    # Send the entire conversation history to the model
+    response = client.chat.completions.create(
+    model=model,
+    messages=new_conversation_history
+    )
+    # Extract the assistant's reply from the response
+    assistant_reply = response.choices[0].message.content
+
+    return assistant_reply
+
+def vision_board_img(prompt):
+    response = client.images.generate(
+        model = "dall-e-3",
+        prompt = "visualize the following children's goal oriented towards a child and try to avoid using text in the image: " + prompt,
+        size = "1024x1024",
+        quality='standard',
+        n=1
+    )
+    image_url = response.data[0].url
+    return image_url
+
 
