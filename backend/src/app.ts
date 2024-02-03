5f91f31823c24913d783bd94d2e9d36360162ce4
Oliver Dantzer <oliverdantzer@gmail.com>
2024-02-03 14:58:30 -0500

    Remove auth backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,91 +14,10 @@ import { messagesRouter } from "./messages/messages.router";
 import { errorHandler } from "./middleware/error.middleware";
 import { notFoundHandler } from "./middleware/not-found.middleware";
 
-if (!(process.env.PORT && process.env.CLIENT_ORIGIN_URL)) {
-  throw new Error(
-    "Missing required environment variables. Check docs for more info."
-  );
-}
-
-const PORT = parseInt(process.env.PORT, 10);
-const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL;
 
 const app = express();
-const apiRouter = express.Router();
 
-app.use(express.json());
-app.set("json spaces", 2);
-
-app.use(
-  helmet({
-    hsts: {
-      maxAge: 31536000,
-    },
-    contentSecurityPolicy: {
-      useDefaults: false,
-      directives: {
-        "default-src": ["'none'"],
-        "frame-ancestors": ["'none'"],
-      },
-    },
-    frameguard: {
-      action: "deny",
-    },
-  })
-);
-
-app.use((req, res, next) => {
-  res.contentType("application/json; charset=utf-8");
-  next();
-});
-app.use(nocache());
-
-// app.get(`/api/`, (req, res) => {
-//   child_process.exec('ls', (error, stdout, stderr) => {
-//   res.json({ message: "Hello from the server!" });
-// }
-
-const { exec } = require('child_process');
-
-// Path to your Python script
-const pythonScriptPath = 'openai-and-opencv/opencv/main.py';
-
-// Asks first question
-// Execute the Python script
-const first_question = 'q1'
-const second_response = 'r2'
-const third_response = 'r3'
-const fourth_response = 'r4'
-const fifth_response = 'r5'
-const sixth_response = 'r6'
-
-exec(`python ${pythonScriptPath} "${first_question}"`, (error: Error, stdout : string, stderr : string) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-
-  // Output from the script
-  console.log(`stdout: ${stdout}`);
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-  }
-});
-
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN_URL,
-    methods: ["GET"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-    maxAge: 86400,
-  })
-);
-
-app.use("/api", apiRouter);
-apiRouter.use("/messages", messagesRouter);
-
-app.use(errorHandler);
-app.use(notFoundHandler);
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
